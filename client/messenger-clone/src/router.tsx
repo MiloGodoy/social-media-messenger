9cea9507ae6a6b330a361a7beb08ca6362640598
Milo Godoy <milogodoy@hotmail.com>
2024-04-25 14:26:58 -0400

    creating project

---
+++ client/messenger-clone/src/router.tsx
@@ -0,0 +1,41 @@
+import { Outlet, createBrowserRouter } from "react-router-dom";
+import { AuthLayout } from './pages/layouts/AuthLayout';
+import { Login } from "./pages/Login";
+import { Signup } from './pages/Signup';
+import { AuthProvider } from "./context/AuthContext";
+import { Home } from "./pages/Home";
+import { RootLayout } from "./pages/layouts/RouteLayout";
+import { NewChannel } from "./pages/channel/New";
+
+export const router = createBrowserRouter([
+    {
+        element: <ContextWrapper />,
+        children: [
+            {
+                path: "/",
+                element: <RootLayout />,
+                children: [
+                    { index: true, element: <Home /> },
+                    { path: "/channel", children: [
+                        { path: "new", element: <NewChannel />}
+                    ]}
+                ],
+            },
+            {
+                element: <AuthLayout />,
+                children: [
+                    { path: "login", element: <Login /> },
+                    { path: "signup", element: <Signup /> }
+                ],
+            },
+        ],
+    },
+]);
+
+function ContextWrapper () {
+    return(
+        <AuthProvider>
+            <Outlet />
+        </AuthProvider>
+    )
+}
